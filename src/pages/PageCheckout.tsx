7c06b402e0210f7451aa98a62d395a95d4ae9e50
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-27 00:43:59 +0100

    finished

--- src/pages/PageCheckout.tsx
+++ src/pages/PageCheckout.tsx
@@ -2,16 +2,28 @@ import { useContext } from "react";
 import { AppContext } from "../AppContext";
 
 export const PageCheckout = () => {
-	const { userName } = useContext(AppContext);
+	const { userName, cart } = useContext(AppContext);
 
 	return (
 		<>
 			{userName && (
-				<p>
+				<p className="mb-4">
 					{userName}, you are one step away from your books being
 					delivered!
 				</p>
 			)}
+			{cart.items.map(techBook => {
+				return (
+					<div className="flex gap-3 mb-3 items-center">
+							<img
+								className="h-fit w-12 cursor-pointer"
+								key={techBook.id}
+								src={`https://edwardtanguay.vercel.app/share/images/techBooks/${techBook.idCode}.jpg`}
+							/>
+						<p className="text-3xl">{techBook.title}</p>
+					</div>
+				)
+			})}
 		</>
 	);
 };
